d9c83814e73a9ca9dfa6de6329d74ec5d2bba04e
TommysG <thomgian@csd.auth.gr>
2018-12-10 22:34:30 +0200

    added settings

--- src/sample/MainMenu.java
+++ src/sample/MainMenu.java
@@ -1,11 +1,15 @@
 package sample;
 
+import javafx.animation.ScaleTransition;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 import java.io.IOException;
 
@@ -14,10 +18,26 @@ public class MainMenu {
     @FXML
     private Button play;
 
+    public void initialize(){
+
+    }
+
     public void playClicked() throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("Game.fxml"));
         Stage stage = (Stage) play.getScene().getWindow();
         stage.setScene(new Scene(root,800,600));
 
+    }
+
+    public void settingsClicked(){
+
+    }
+
+    public void creditsClicked(){
+
+    }
+
+    public void exitClicked(){
+        Platform.exit();
     }
 }
