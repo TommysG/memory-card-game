5fdd2600497635548126e69f89d170bed5bd9c42
TommysG <thomgian@csd.auth.gr>
2018-12-10 22:35:30 +0200

    added settings

--- src/sample/MainMenu.java
+++ src/sample/MainMenu.java
@@ -1,15 +1,12 @@
 package sample;
 
-import javafx.animation.ScaleTransition;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
-import javafx.util.Duration;
 
 import java.io.IOException;
 
