f7204f4ba5928fa9f9ce6a37e8907030574932b5
TommysG <thomgian@csd.auth.gr>
2018-12-10 21:33:59 +0200

    added back button

--- src/sample/Game.java
+++ src/sample/Game.java
@@ -1,4 +1,22 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class Game {
+
+    @FXML
+    private Button back;
+
+    public void backClicked() throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("MainMenu.fxml"));
+        Stage stage = (Stage) back.getScene().getWindow();
+        stage.setScene(new Scene(root,800,600));
+    }
 }
